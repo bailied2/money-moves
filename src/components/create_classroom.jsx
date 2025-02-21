865b20dd2299b150db436dba095316e6289777fb
Devin Bailie <bailied2@winthrop.edu>
2025-02-21 12:48:45 -0500

    reorganizing files

---
+++ src/components/create_classroom.jsx
@@ -0,0 +1,38 @@
+export default function CreateClassroom() {
+  return (
+    <form id="create_classroom_form" action="">
+      <fieldset>
+        <legend>Create Classroom</legend>
+        <label
+          >Classroom Name:
+          <input
+            type="text"
+            id="create_classroom_name"
+            name="create_classroom_name" /></label
+        ><br />
+        <label
+          >Start Date:
+          <input
+            type="date"
+            id="classroom_start_date"
+            name="classroom_start_date" /></label
+        ><br />
+        <label
+          >End Date:
+          <input
+            type="date"
+            id="classroom_end_date"
+            name="classroom_end_date" /></label
+        ><br />
+        <input
+          type="checkbox"
+          id="classroom_savings"
+          name="classroom_savings"
+          checked
+        /><label for="classroom_savings">Savings Enabled</label><br />
+        <input type="submit" value="Create Classroom" />
+      </fieldset>
+    </form>
+  );
+}
+  
