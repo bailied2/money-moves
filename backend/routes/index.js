e906637bf1970b2a11da12994ef93c1fe017bddf
K. Goff <goffk3>
2025-02-21 14:21:09 -0500

    moved routes folder to backend

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 8080; // Choose a port for your server
+
+// Middleware
+app.use(express.json()); // Parses JSON requests
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:8080`);
+});
